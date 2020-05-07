7015df7e778d3139ba1d8d541ff80cb9f6b6d56b
tejaskochar <tejaskochar95@gmail.com>
2020-05-08 00:10:41 +0530

    Final

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField(max_length=200, blank=True)
+    pub_date = models.DateTimeField()
+    tot_votes = models.IntegerField(default=1)
+    image = models.ImageField(upload_to = 'product/')
+    icon = models.ImageField(upload_to = 'product/')
+    body = models.TextField()
+    hunter = models.ForeignKey(User, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:200]
+
+    def pub_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
